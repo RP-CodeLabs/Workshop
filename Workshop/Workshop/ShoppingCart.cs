e89eeca32c9a1c1ac9021674168956ef4c7af3f5
RP-CodeLabs <raajponnam@gmail.com>
2017-06-29 18:10:55 +0100

    Added foreach to calculate multiple postage items

--- Workshop/Workshop/ShoppingCart.cs
+++ Workshop/Workshop/ShoppingCart.cs
@@ -19,19 +19,22 @@ namespace Workshop
         public decimal PostalPriceCalculation(PostageItem postageItem)
         {
             var total = 0m;
-            switch (postageItem.DeliveryType)
+            foreach (var postageItem in PostageItems)
             {
-                case "24hrs":
-                    total += postageItem.Weight * 4m;
-                    break;
-                case "48hrs":
-                    total += postageItem.Weight * 6m;
-                    break;
-                case "SpecialDelivery":
-                    total += postageItem.Weight * 6m;
-                    var extra = postageItem.Weight / 2;
-                    total += extra * 0.55m;
-                    break;
+                switch (postageItem.DeliveryType)
+                {
+                    case "24hrs":
+                        total += postageItem.Weight * 4m;
+                        break;
+                    case "48hrs":
+                        total += postageItem.Weight * 6m;
+                        break;
+                    case "SpecialDelivery":
+                        total += postageItem.Weight * 6m;
+                        var extra = postageItem.Weight / 2;
+                        total += extra * 0.55m;
+                        break;
+                }
             }
             return total;
         }
